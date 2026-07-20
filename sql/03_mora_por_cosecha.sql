e9701e6124013ea0e0da2bc77af3b6fdb72b0b84
jimmysanz92-netizen <jimmysanz92@gmail.com>
2026-07-19 19:30:38 -0500

    v 1.5
    Refactor query to improve counting of overdue credits and percentage calculation.

--- sql/03_mora_por_cosecha.sql
+++ sql/03_mora_por_cosecha.sql
@@ -1,12 +1,16 @@
 SELECT CONVERT(char(7), cr.fecha_desembolso, 126)   AS cosecha,
        COUNT(DISTINCT cr.id_credito)                AS creditos,
-       SUM(CASE WHEN cu.fecha_pago_real IS NULL
+       COUNT(DISTINCT CASE WHEN cu.fecha_pago_real IS NULL
                  AND cu.fecha_vencimiento <= '2024-12-31'
-                THEN 1 ELSE 0 END)                  AS creditos_en_mora,
-       1.0 * SUM(CASE WHEN cu.fecha_pago_real IS NULL
+                THEN cr.id_credito
+                ELSE NULL END)                  AS creditos_en_mora,
+
+       100.0 * COUNT(DISTINCT CASE WHEN cu.fecha_pago_real IS NULL
                        AND cu.fecha_vencimiento <= '2024-12-31'
-                      THEN 1 ELSE 0 END)
+                      THEN cr.id_credito
+                      ELSE NULL END)
              / COUNT(DISTINCT cr.id_credito)        AS pct_en_mora
+
 FROM creditos cr
 JOIN cuotas cu ON cu.id_credito = cr.id_credito
 GROUP BY CONVERT(char(7), cr.fecha_desembolso, 126)
